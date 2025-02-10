c437f8eafd2673196c5bffb5a31d321093e17b70
Rahmanhusain <rahmanhusain899@gmail.com>
2025-02-10 14:41:34 +0530

    some changes

--- client/src/components/ImagePic.jsx
+++ client/src/components/ImagePic.jsx
@@ -12,7 +12,7 @@ export default function ImagePic() {
         </div>
         <div className="mainImg h-full">
           <Image src={girl} alt='girlimage' className='rounded-2xl w-auto h-[50vh] brightness-75' />
-          <h2 className='cookie text-5xl mt-4'>Lisa</h2>
+          <h2 className='MervaleScript text-5xl mt-4'>Lisa</h2>
           <div className='flex flex-row items-center justify-start gap-4 '>
             <h3 className='bg-[#FF006Aa7] text-sm  p-1 px-4 rounded-full'>hello</h3>
             <h3>hello</h3>
