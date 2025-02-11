5e08f0761b2c13374daa3fd9a1bd57e2fe0b976c
Aditya-tech1 <adityatech1m@gmail.com>
2025-02-11 12:45:33 +0530

    fadeout effect

--- client/src/components/ImagePic.jsx
+++ client/src/components/ImagePic.jsx
@@ -1,22 +1,36 @@
+'use client';
 import React from "react";
 import Image from "next/image";
-import girl from "../../public/bla.jpg";
 import { Heart, HeartBroken } from "@/icons/icon";
 
+const fadeout=()=> {
+  document.querySelector(".girlImage").classList.add('scale-90');
+  document.querySelector(".girlImage").classList.add('opacity-0');
+  setInterval(() => {
+    document.querySelector(".girlImage").classList.add('hidden');
+  }, 2000);
+};
+
+
+
 export default function ImagePic() {
+
+
   return (
     <div className="px-6">
-      <div className="flex flex-row items-center justify-evenly min-h-[100dvh]">
-        <div className="heartbreak fixed top-1/2 left-1/4 bg-[#A100FF40] rounded-full p-4">
+      <div className="flex flex-row items-center justify-evenly min-h-[100dvh] ">
+        <div id="heartbreakIcon" className="heartbreak fixed top-1/2 left-1/4 bg-[#A100FF40] rounded-full cursor-pointer p-4" onClick={fadeout}>
           <HeartBroken className="w-12 h-12 text-[#E1306C]" />
         </div>
 
-        <div className="mainImg h-full max-w-96 flex flex-col items-start justify-center ">
-          <div className="mx-auto">
-            <Image
-              src={girl}
+        <div className="mainImg h-full max-w-96 flex flex-col items-start justify-center girlImage  transition-all duration-1000 ease-in-out">
+          <div className="mx-auto ">
+            <Image id="girlImg"
+              src='/bla.jpg'
               alt="girlimage"
-              className="rounded-2xl h-[55vh] w-auto aspect-[39/49] object-cover  brightness-75"
+              width={300}
+              height={500}
+              className=" rounded-2xl h-[55vh] w-auto aspect-[39/49] object-cover  brightness-75"
             />
             <h2 className="cookie text-4xl mt-4 italic">Lisa</h2>
             <div className="flex flex-row items-center justify-start gap-2 MervaleScript">
@@ -42,10 +56,10 @@ export default function ImagePic() {
           </p>
         </div>
 
-        <div className="heartbreak fixed top-1/2 right-1/4  bg-[#FF006A40] rounded-full p-4">
+        <div id="heartIcon" className="heartbreak fixed top-1/2 right-1/4  bg-[#FF006A40] rounded-full p-4">
           <Heart className="w-12 h-12 text-[#E1306C]" />
         </div>
-        
+
       </div>
       <div id="hearts-alpaca" className="hearts fixed bottom-0 left-0 opacity-50 sm:opacity-100">
         <div className="heart"></div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { BellIcon } from '@/icons/icon'
 import React from 'react'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
@@ -7,18 +8,20 @@ export default function Navbar() {
       <h1 className='text-3xl sm:text-4xl cookie '>
         Find Your Date
       </h1>
-      <div className='flex flex-row items-center justify-evenly gap-3 w-fit'>
+      <div className='flex flex-row items-center justify-evenly gap-3 w-fit hidden'>
         <div className="notify">
-          <BellIcon className='w-7 h-7 text-white'/>
+          <BellIcon className='w-7 h-7 text-white' />
         </div>
         <div className="profile">
           {/* <img src="#" alt="#" /> */}
-          <BellIcon className='w-7 h-7 text-white'/>
-          
-
+          <BellIcon className='w-7 h-7 text-white' />
         </div>
 
       </div>
+      <div className='flex flex-row items-center justify-evenly gap-3 w-fit '>
+        <Link href="/register" className='p-2 px-8 rounded bg-[#bd145b]'>SignUp</Link>
+      </div>
+
     </nav>
   )
 }
